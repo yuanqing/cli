546fb2e86fba4fd71e17c3c7563c8bdb2266348d
yuanqing <desk@yuanqing.sg>
2020-11-10 23:34:29 +0800

    Add README.md and example

---
+++ example/single-command-cli.ts
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+import { createCli } from '@yuanqing/cli'
+
+const config = {
+  name: 'my-cli',
+  version: '1.0.0'
+}
+const commandConfig = {
+  positionals: [
+    {
+      type: 'STRING',
+      name: 'files',
+      description: 'Glob of input files.',
+      required: true,
+    }
+  ],
+  options: [
+    {
+      type: 'BOOLEAN',
+      name: 'minify',
+      aliases: ['m'],
+      description: 'Whether to minify the output.',
+      default: false
+    },
+    {
+      type: 'STRING',
+      name: 'output',
+      aliases: ['o'],
+      description: "Set the output directory. Defaults to './build'.",
+      default: './build'
+    },
+    {
+      type: 'NON_ZERO_POSITIVE_INTEGER',
+      name: 'parallel',
+      aliases: ['p'],
+      description: "Set the maximum number of files to process concurrently. Defaults to '3'.",
+      default: 3
+    }
+  ],
+  examples: [
+    "'*' --minify",
+    "'*' --output './dist'",
+    "'*' --parallel 10"
+  ]
+}
+
+try {
+  const result = createCli(config, commandConfig)(process.argv.slice(2))
+  if (typeof result !== 'undefined') {
+    const { positionals, options, remainder } = result
+    console.log(positionals) //=> { files: '*' }
+    console.log(options)     //=> { minify: true, output: './dist', parallel: 10 }
+    console.log(remainder)   //=> [ 'foo', 'bar' ]
+  }
+} catch (error) {
+  console.error(`my-cli: ${error.message}`)
+  process.exit(1)
+}
